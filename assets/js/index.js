7221bb990e34807cb8280f78742b4c83c8f9ae73
mikepreston17 <michael.n.preston@gmail.com>
2018-11-02 14:10:47 -0500

    pre-visuals commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,10 +14,11 @@ var config = {
 firebase.initializeApp(config);
 
 var database = firebase.database();
-var dbRef = database.ref();
-var connectionsRef = database.ref("/connections");
-var clientIP;
 
+var connectionsRef = database.ref("/connections");
+var clientConnectionRef = database.ref(".info/connected");
+
+var clientIP, clients, roomsRef, clientRef;
 
 window.onload = init;
 
@@ -25,78 +26,95 @@ function init() {
 
     let roomName = "tinytiger";
 
+    $('<button>')
+        .text('leave server')
+        .appendTo($('#score'))
+        .on('click', leaveRoom)
+
+    // $('#player-hand').append($("<i class='fa fa-eye'></i>")).button();
+
     getMyIPAsync().then(result => {
-        clientIP = result;
-        console.log('client ip: ', clientIP);
-        var player = {
-            name: "Michael",
-            ip: clientIP,
-        }
 
-        clearAllRooms();
+            clientIP = result;
+            console.log('client ip: ', clientIP);
 
-        addPlayerToRoom(player, roomName);
-        addCPUToRoom(roomName);
-        render();
-    })
+            var player = {
+                name: "Michael",
+                ip: clientIP,
+                choice: null,
+            }
 
+            clearAllRooms(); //todo: remove before prod
+
+            addPlayerToRoom(player, roomName);
+            addCPUToRoom(roomName);
+            render();
+
+        }).then(() => {
+            // track connected user:
+            clients = database.ref(".info/connected")
+                .on("value", snapshot => {
+
+                    if (snapshot.val() && clientIP) {
+
+                        //Adding the user's IP address to prevent multiple tabs.
+                        console.log('uip: ', clientIP);
+                        var connection = connectionsRef.push({
+                            uip: clientIP
+                        });
+
+                        connection.onDisconnect().remove(
+                            error => {
+                                if (error) console.log('could not disconnect: ', error);
+                            })
+                    }
+                });
+        })
+        .then(() => connectionsRef.on("value", snapshot => $("#watchers").text(snapshot.numChildren())))
+        .then(() => {
+            // On rooms updated:
+            roomsRef = database.ref("rooms").on("value", snapshot => console.log('rooms snapshot() ', snapshot.val()))
+        }).then(() => {
+            // clientRef = database.ref(`${clientIP}`);
+        })
 }
 
-//test
-// connectionsRef.push(clientIP);
-
-const render = () => {
+const render = async () => {
     // Show:
     //   who is in the room.
-    $('#who-is-here')
-        .append(`<li><p>Your IP: ${clientIP}</p></li>`);
+    $('#who-is-here').append(`<li><p>Your IP: ${clientIP}</p></li>`);
 
-    //   whose turn it is.
-    database.ref("turnPlayer").on("value", snap => {
-        console.log('turn player: ', snap);
-    });
+    // user choice / opponent choice.
 
-    //   user choice / opponent choice.
-    
 }
-
-
-var clients = database.ref(".info/connected")
-    .on("value", function (snap) {
-        if (snap.val() && clientIP) {
-            // Add user to the connections list.
-            //TODO: add the user's IP address to prevent multiple tabs.
-            // console.log('uip: ', clientIP);
-            var con = connectionsRef.push({
-                uip: clientIP //todo: figure out why this sometimes gets undefined / ensure the awaited clientIP finished before this listener even initializes.
-            });
-            con.onDisconnect().remove();
-        }
-    });
-
-//create the rooms ref and persist it, outright, with code or manually...then push items to it.
-var roomsRef = database.ref("rooms").on("value", function (snapshot) {
-    console.log('snapshot() ', snapshot);
-})
 
 const addCPUToRoom = (roomName) => {
     var cpuRef = firebase.database().ref('rooms/' + roomName).push({
         id: '11.011.100.101',
         name: "CPU",
+        choice: null,
     });
     console.log('cpu ref: ', cpuRef);
 }
 
-const addPlayerToRoom = (user, roomName) => {
-    // console.log('ADDING USER TO ROOM');
-    // console.log('user: ', user);
-    firebase.database().ref('rooms/' + roomName).push(user);
+const leaveRoom = () => {
+    firebase.database()
+        .ref("connections/")
+        .child(`${clientIP}`)
+        .remove();
 }
 
-const clearAllRooms = () => firebase.database().ref('rooms').remove();
+const addPlayerToRoom = (user, roomName) => firebase.database().ref('rooms/' + roomName).push(user);
+
+const clearAllRooms = async () => firebase.database().ref('rooms').remove();
 
 const getMyIPAsync = async () => await getMyIP();
+
 const getMyIP = () => new Promise((resolve, reject) => $.getJSON('https://ipapi.co/json', data => resolve(data.ip)))
+
 var wait = ms => new Promise((r, j) => {
     setTimeout(r, ms)
 })
+
+const random = (min, max, inclusive) =>
+    Promise.resolve(Math.floor(Math.random() * (max - min + (inclusive ? 1 : 0))) + min);
